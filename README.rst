c978d7209406fc0fe41fbb846f9e33e01a3a82eb
Steven Myint <git@stevenmyint.com>
2012-11-18 06:59:53 -0800

    Add descriptions

--- README.rst
+++ README.rst
@@ -1,3 +1,21 @@
+====
+trim
+====
+
+Trims trailing whitespace from files.
+
 .. image:: https://secure.travis-ci.org/myint/trim.png
    :target: https://secure.travis-ci.org/myint/trim
    :alt: Build status
+
+-----
+Usage
+-----
+
+Trim trailing whitespace on a single file::
+
+   $ trim example.py
+
+Recursively::
+
+   $ trim .
--- setup.py
+++ setup.py
@@ -4,6 +4,27 @@
 from distutils import core
 
 
-core.setup(name='trim',
-           author='myint',
-           scripts=['trim'])
+def version():
+    """Return version string."""
+    with open('trim') as input_file:
+        for line in input_file:
+            if line.startswith('__version__'):
+                import ast
+                return ast.literal_eval(line.split('=')[1].strip())
+
+
+with open('README.rst') as readme:
+    core.setup(name='trim',
+               version=version(),
+               description='Trims trailing whitespace from files.',
+               long_description=readme.read(),
+               license='Expat License',
+               author='myint',
+               url='https://github.com/myint/trim',
+               classifiers=['Intended Audience :: Developers',
+                            'Environment :: Console',
+                            'Programming Language :: Python :: 2',
+                            'Programming Language :: Python :: 3',
+                            'License :: OSI Approved :: MIT License'],
+               keywords='trim, format, trailing, whitespace',
+               scripts=['trim'])
