aa4c7ca3c4a071d0237ae4fc9378b9b26fc4e533
myint <macbookpro.local>
2012-12-15 17:19:32 -0800

    Add a system test

--- test_trim.py
+++ test_trim.py
@@ -45,6 +45,26 @@ test
     def test_is_text_should_consider_symlinks_as_non_text(self):
         self.assertFalse(trim.is_text(os.path.join(ROOT_DIR, 'trim.py')))
 
+    def test_system(self):
+        text = 'abc   \n   1234  \n\n  \n'
+        import tempfile
+        with tempfile.NamedTemporaryFile(delete=False,
+                                         mode='w') as temporary_file:
+            temporary_file.write(text)
+
+        import subprocess
+        process = subprocess.Popen([os.path.join(ROOT_DIR, 'trim'),
+                                    temporary_file.name],
+                                   stderr=subprocess.PIPE)
+        process.communicate()
+        self.assertEqual(0, process.returncode)
+
+        with open(temporary_file.name) as input_file:
+            self.assertEqual(trim.trim(text),
+                             input_file.read())
+
+        os.remove(temporary_file.name)
+
 
 if __name__ == '__main__':
     unittest.main()
